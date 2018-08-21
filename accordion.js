dfc897d7a8dfebfb390e983233af20628fee268b
Dawid Mariankowski <dawid24.9@o2.pl>
2018-08-21 14:27:34 +0200

    Create accordion.js

---
+++ accordion.js
@@ -0,0 +1,91 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+
+(function () { 
+
+document.querySelector("#tab1-0").addEventListener("click", function(event) {
+    
+
+     for(var i =0; i<=5; i++){
+                if(document.getElementsByClassName('gs-faq__answer')[0].style.display == 'block'){
+                    continue;
+                }else{
+        document.getElementsByClassName('gs-faq__answer')[i].style.display = 'none';
+                }
+    
+ 
+}     
+event.preventDefault();
+}, false);
+document.querySelector("#tab2-1").addEventListener("click", function(event) {
+
+
+    for(var i =0; i<=5; i++){
+        if(document.getElementsByClassName('gs-faq__answer')[1].style.display == 'block'){
+            continue;
+        }else{
+document.getElementsByClassName('gs-faq__answer')[i].style.display = 'none';
+        }
+
+
+}           
+event.preventDefault();
+}, false);
+document.querySelector("#tab3-2").addEventListener("click", function(event) {
+ 
+    for(var i =0; i<=5; i++){
+        if(document.getElementsByClassName('gs-faq__answer')[2].style.display == 'block'){
+            continue;
+        }else{
+document.getElementsByClassName('gs-faq__answer')[i].style.display = 'none';
+        }
+
+
+}           
+event.preventDefault();
+}, false);
+document.querySelector("#tab4-3").addEventListener("click", function(event) {
+    
+    for(var i =0; i<=5; i++){
+        if(document.getElementsByClassName('gs-faq__answer')[3].style.display == 'block'){
+            continue;
+        }else{
+document.getElementsByClassName('gs-faq__answer')[i].style.display = 'none';
+        }
+
+
+}           
+event.preventDefault();
+}, false);
+document.querySelector("#tab5-4").addEventListener("click", function(event) {
+ 
+    for(var i =0; i<=5; i++){
+        if(document.getElementsByClassName('gs-faq__answer')[4].style.display == 'block'){
+            continue;
+        }else{
+document.getElementsByClassName('gs-faq__answer')[i].style.display = 'none';
+        }
+
+
+}             
+event.preventDefault();
+}, false);
+
+document.querySelector("#tab6-5").addEventListener("click", function(event) {
+
+    for(var i =0; i<=5; i++){
+        if(document.getElementsByClassName('gs-faq__answer')[5].style.display == 'block'){
+            continue;
+        }else{
+document.getElementsByClassName('gs-faq__answer')[i].style.display = 'none';
+        }
+
+
+}        
+    event.preventDefault();
+    }, false);
+
+}());
+
+
+});
